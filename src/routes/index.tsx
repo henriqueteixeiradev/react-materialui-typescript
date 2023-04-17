20a4da180c47ccb6b20851b54857e313faaf5662
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2023-04-17 10:45:13 -0300

    feat: add material ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { NotFound } from "../pages/notfound";
+import { Button } from "@mui/material";
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={<Button>asdasd</Button>} />
       <Route path="/notfound" element={<NotFound />} />
       <Route path="*" element={<Navigate to="/notfound" />} />
     </Routes>
